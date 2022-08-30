d5a43814440500d63b7b8480fcb9c7148d4b32ed
lwl110 <1916403680@qq.com>
2022-08-30 16:47:58 +0800

    8.30-1.2

---
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -0,0 +1,17 @@
+package com.itheima.reggie;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@ServletComponentScan
+@EnableTransactionManagement
+@MapperScan(basePackages = "com.itheima.reggie.mapper")
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class, args);
+    }
+}
